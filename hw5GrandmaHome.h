ddf340a857d8f867b1e0cbdf7a5275d968b7dc97
user <github.ecj8p@8alias.com>
2022-12-03 14:25:41 -0800

    update

--- hw5GrandmaHome.h
+++ hw5GrandmaHome.h
@@ -1,19 +1,18 @@
 #pragma once
-
-#include<fstream>
-#include<iostream>
-
+#include <iostream>
 #include <stdio.h>
 #include <iomanip>
 #include <cstdlib>
+
+// for Json::value
 #include <json/json.h>
 #include <json/reader.h>
 #include <json/writer.h>
 #include <json/value.h>
-#include <string>
-#include "Record.h"
+#include <string.h>
+#include "Parameter.h"
+class Location : public Parameter {
+    public:
+};
 
 
-
-void myPrintLog(Record s1);
-char * myFile2String(char *f_name);
---
+++ hw5Home.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<fstream>
+
+#include <stdio.h>
+#include <iomanip>
+#include <cstdlib>
+#include <json/json.h>
+#include <json/reader.h>
+#include <json/writer.h>
+#include <json/value.h>
+#include <string.h>
+
+#include "Parameter.h"
+#include "Person.h"
+#include "Time.h"
+#include "Thing.h"
+#include "Location.h"
+#include "Record.h"
+#include "hw3json.h"
+
+using namespace std;
+
+int main() {
+  Location hm;
+  hm.name = "home";
+
+  return 0;
+}
